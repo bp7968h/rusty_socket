a7cd65b7814daae31a059ff00aa43d783cd3b487
Bhuwan Pandit <bp7968h@gre.ac.uk>
2024-08-01 23:18:28 +0100

    Add Opcode and implement from trait

---
+++ rusty_socket_core/src/opcode.rs
@@ -0,0 +1,38 @@
+#[derive(Debug, PartialEq)]
+pub enum OpCode{
+    ContinuationFrame,
+    Text,
+    Binary,
+    ConnectionClose,
+    Ping,
+    Pong,
+    Unknown,
+}
+
+impl From<u8> for OpCode {
+    fn from(value: u8) -> Self {
+        match value {
+            0 => OpCode::ContinuationFrame,
+            1 => OpCode::Text,
+            2 => OpCode::Binary,
+            8 => OpCode::ConnectionClose,
+            9 => OpCode::Ping,
+            10 => OpCode::Pong,
+            _ => OpCode::Unknown,
+        }
+    }
+}
+
+impl From<OpCode> for u8 {
+    fn from(value: OpCode) -> Self {
+        match value {
+            OpCode::ContinuationFrame => 0,
+            OpCode::Text => 1,
+            OpCode::Binary => 2,
+            OpCode::ConnectionClose => 8,
+            OpCode::Ping => 9,
+            OpCode::Pong => 10,
+            OpCode::Unknown => 255
+        }
+    }
+}
